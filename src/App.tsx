e2bf01d2d221b5a467bbb7fa81000528e1885aae
Fynn Knoop <fknoop@techfak.de>
2021-10-21 11:16:06 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, ImmutableXClient } from '@imtbl/imx-sdk';
+import { useState } from 'react';
 
 function App() {
+  async function sdkExample() {
+    const imxAddress = '0x4764bc088a27f490353e8cf1558ba02fdc418c65';
+    const link = new Link('https://link.x.immutable.com');
+
+    // Register user, you can persist address to local storage etc.
+    const { address } = await link.setup({});
+    console.log(address);
+    const client = await ImmutableXClient.build({ publicApiUrl: 'https://api.x.immutable.com/v1' });
+    const assets = await client.getAssets({
+            user: address,
+            collection: imxAddress
+    });
+    console.log(assets);
+    setAssets(assets.result);
+  }
+
+  const [assets, setAssets] = useState([] as any);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={sdkExample}>TEST</button>
+        <p>You own: {assets.length} items.</p>
       </header>
     </div>
   );
